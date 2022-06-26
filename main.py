e523df04b2347905ca1fff01590377333738c26e
Daniel <pikachungg@gmail.com>
2022-06-26 05:02:23 -0400

    app working!

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from logging.config import listen
 from pynput.keyboard import Key, Listener
 import pyautogui
+import pyperclip
 
 replacements = {
-    "skull": "penis",
-    "eyes": "asdfom"
+    "skull": "💀",
+    "eyes": "👀",
+    "smile": "🙂",
+    "clown": "🤡",
+    "fire": "🔥",
+    "heart": "❤️"
 }
 
 macro_starter = ":"
@@ -19,18 +24,30 @@ def on_press(key):
     key_str = str(key).replace('\'', '')
 
     if listening: 
+        if (key == Key.enter):
+            listening = False
+        if (key == Key.backspace): 
+            if len(typed_keys) < 1:
+                listening = False
+            else:
+                typed_keys.pop()
         if key_str.isalpha() and key_str != macro_ender:
             typed_keys.append(key_str)
         if key_str == macro_ender:
             print("end")
             candidate_keyword = ""
             candidate_keyword = candidate_keyword.join(typed_keys)
-
+            
             if candidate_keyword != "":
-                if candidate_keyword in replacements.keys():
-                    print(candidate_keyword)
+                if candidate_keyword == 'quit':
                     pyautogui.press('backspace', presses=len(candidate_keyword) + 2)
-                    pyautogui.typewrite(replacements[candidate_keyword])
+                    quit()
+                if candidate_keyword in replacements.keys():
+                    pyautogui.press('backspace', presses=len(candidate_keyword) + 2)
+                    pyperclip.copy(replacements[candidate_keyword])
+                    pyautogui.hotkey('ctrl', 'v')
+                    listening = False
+                else:
                     listening = False
     elif key_str == macro_starter and listening == False:
         print("start")
